1d6db2ec7b37c604a0e3218460c9aa24b78b47d5
zaplon <janek.zapal@gmail.com>
2016-12-29 15:10:41 +0100

    Update context_processors.py

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,5 +1,5 @@
 from utils.forms import NoFormFormHelper
-
+from user_profile.forms import DoctorForm
 
 def form_helpers(request):
     return {'NoFormFormHelper': NoFormFormHelper}
@@ -7,7 +7,11 @@ def form_helpers(request):
 
 def utils(request):
     if request.user.is_authenticated and hasattr(request.user, 'doctor'):
-        return {'recipes_available': request.user.doctor.recipes.filter(was_used=False).count(),
+        user = request.user
+        doctor = request.user.doctor
+        profile_form = DoctorForm(initial={'first_name': user.first_name, 'last_name': user.last_name, 'pwz': doctor.pwz,
+                    'email': user.email})
+        return {'recipes_available': request.user.doctor.recipes.filter(was_used=False).count(), 'profile_form': profile_form,
                 'recipes_total': request.user.doctor.recipes.count()}
     else:
         return {}
