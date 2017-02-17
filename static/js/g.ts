04aaa5f161aa0b9ea693cd90ded7215789228fca
zaplon <janek.zapal@gmail.com>
2017-02-17 12:39:43 +0100

    Update g.ts

--- static/js/g.ts
+++ static/js/g.ts
@@ -53,8 +53,8 @@ class Gabinet {
                 ${body}
               </div>
               <div class="modal-footer">
-                ${save}
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
+                ${save}
               </div>
             </div>
           </div>
@@ -75,6 +75,52 @@ class Gabinet {
             var url = `/static/forms/${form}/${params_str}`;
         this.showPdf(url);
     };
+    addRecipes(){
+        var body = $('#addRecipeForm').html();
+        var save = '<button class="btn btn-primary" id="addRecipes">Dodaj</button>';
+        this.showModal('Dodaj numery recept', body, save);
+        $('#addRecipes').click(function(){
+            $.ajax({
+                // Your server script to process the upload
+                url: 'profile/add_recipe/',
+                type: 'POST',
+
+                // Form data
+                data: new FormData($('#addRecipeForm')[0]),
+
+                // Tell jQuery not to process data or worry about content-type
+                // You *must* include these options!
+                cache: false,
+                contentType: false,
+                processData: false,
+                
+                success: function(res){
+                    if (res.success){
+                        $('button.close').click();
+                    }
+                    else {
+                        $('#recipe-errors').html(res.errors);
+                    }
+               }, 
+                
+                // Custom XMLHttpRequest
+                xhr: function() {
+                    var myXhr = $.ajaxSettings.xhr();
+                    if (myXhr.upload) {
+                        // For handling the progress of the upload
+                        myXhr.upload.addEventListener('progress', function(e) {
+                            if (e.lengthComputable) {
+                                $('progress').attr({
+                                    value: e.loaded,
+                                    max: e.total,
+                                });
+                            }
+                        } , false);
+                    }
+                    return myXhr;
+                },
+            });
+        }):
 }
 
 var gabinet = new Gabinet();
