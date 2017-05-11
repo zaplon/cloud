f6a28fb3b4b195d987e91ad088fc46cb49176a55
zaplon <janek.zapal@gmail.com>
2017-05-11 12:02:43 +0200

    Update settings.py

--- gabinet/settings.py
+++ gabinet/settings.py
@@ -94,6 +94,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'medicines': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'medicines.sqlite3')
     }
 }
 
