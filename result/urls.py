0b5dafbf5374a030483f02dc836dffc23356d028
zaplon <janek.zapal@gmail.com>
2017-06-01 14:14:58 +0200

    Update urls.py

--- result/urls.py
+++ result/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-
 from .views import *
 
+
 urlpatterns = [
-    url(r'^/archive/new', ResultCreateView.as_view(), name='new-document')
+    url(r'^/archive/new', ResultCreateView.as_view(), name='new-document'),
+    url(r'^/archive/search', search_view, name='search-document')
 ]
