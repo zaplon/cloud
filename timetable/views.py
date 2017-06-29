a6a0931888431ce040c92597780a9588f1e22628
zaplon <janek.zapal@gmail.com>
2017-06-29 15:45:15 +0200

    Update views.py

--- timetable/views.py
+++ timetable/views.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.base import View
 from timetable.models import Term
 from user_profile.models import Doctor
+import json
 
 
 class CalendarView(View):
@@ -32,7 +34,18 @@ def term_move_view(request):
     if not 'id' in request.POST:
         return HttpResponse(status=400)
     t = Term.objects.get(id=request.POST['id'])
-    t.datetime = request.POST['datetime']
-    t.save()
+    t2 = False
+    try:
+        t2 = Term.objects.get(datetime=request.POST['datetime'], doctor=t.doctor)
+    except:
+        pass
+    if t2 and (t2.patient and t.patient):
+        return HttpResponse(json.dumps({'message': 'Na ten termin jest już zapisany pacjent.'}), status=400)
+    if t2 and not t2.patient:
+        t2.patient = t.patient
+        t2.save()
+    else:    
+        t.datetime = request.POST['datetime']
+        t.save()
     return HttpResponse(status=200)
 
