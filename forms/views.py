8ba9fa7606b08d19247755b334a52dcff83311c7
Jan Zapał <janek.zapal@gmail.com>
2018-11-04 00:51:22 +0100

    dev

--- forms/views.py
+++ forms/views.py
@@ -111,7 +111,7 @@ class FormView(PDFTemplateView):
             self.template_name = 'forms/' + template + '.html'
         if 'print' in request.GET:
             project_dir = settings.PROJECT_DIR
-            if template in ['ABA', 'zgoda na znieczulenie']:
+            if template in ['ABA', 'zgoda na znieczulenie', 'karta badania lekarskiego']:
                 style = 'bootstrap_print.css'
             else:
                 style = 'prints.css'
