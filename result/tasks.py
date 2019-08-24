869f943e5d76e095285c170a8d743144cff84f14
Jan Zapał <janek.zapal@gmail.com>
2019-08-24 22:45:23 +0200

    fix

--- result/tasks.py
+++ result/tasks.py
@@ -19,19 +19,22 @@ def generate_results_pdf(patient_id, period):
             start_date = datetime.now() - relativedelta(months=1)
         elif period == '3 months':
             start_date = datetime.now() - relativedelta(months=3)
-        pdf_files = Result.objects.filter(uploaded__gte=start_date, patient_id=patient_id)
+        pdf_files = Result.objects.filter(uploaded__gte=start_date, patient_id=patient_id, file__isnull=False)
     pdfs = []
     redis = get_redis_connection()
     if not pdf_files.exists():
         redis.set(settings.RESULTS_PDF_KEY_PATTERN % (patient_id, period), '-', settings.RESULTS_PDF_TTL)
     for pdf in pdf_files:
-        if pdf.file.path.find('.pdf') > -1:
-            pdfs.append(pdf.file.path)
+        try:
+            if pdf.file.path.find('.pdf') > -1:
+                pdfs.append(pdf.file.path)
+        except ValueError:
+            continue
     output_dir = os.path.join(settings.MEDIA_ROOT, 'merged_results')
     ts = datetime.now().strftime('%s')
     file_name = os.path.join(output_dir, '%s.pdf' % ts)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    subprocess.check_output(["pdftk", ' '.join(pdfs), 'cat', 'output', file_name], stderr=subprocess.STDOUT)
+    subprocess.check_output(["pdftk"] + pdfs + ['cat', 'output', file_name], stderr=subprocess.STDOUT)
     file_url = 'media/merged_results/%s.pdf' % ts
     redis.set(settings.RESULTS_PDF_KEY_PATTERN % (patient_id, period), file_url, settings.RESULTS_PDF_TTL)
